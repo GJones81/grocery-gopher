0a47c3a62fa073f72d87c0b4bd98d9b6d94e2805
gjones81 <jones.guy81@gmail.com>
2020-04-13 10:33:38 -0700

    Implemented homepage and nav-bar content

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//MODULES
 //require needed modules
 let express = require('express')
 let layouts = require('express-ejs-layouts')
@@ -5,6 +6,7 @@ let layouts = require('express-ejs-layouts')
 //create app instance
 let app = express()
 
+//SETTINGS / MIDDLEWARE
 //set template language to ejs
 app.set('view engine', 'ejs')
 
@@ -14,10 +16,20 @@ app.use(layouts)
 //tell express to use the static folder
 app.use(express.static('static'))
 
-//create a 'catch-all' route
+//ROUTES
+
+//create homepage route
+
+app.get('/', (req, res) => {
+	res.render('homepage')
+})
+
+
+//create a 'catch-all' route, last inline
 app.get('*', (req, res) => {
 	res.render('error')
 })
 
+//
 //pick a port to listen on
 app.listen(3000)
